27dbf276af4c576a35ff83f5ba9309643ad73965
hmccarthy1 <huntermccarthy56@hotmail.com>
2024-02-25 20:49:16 -0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import styles from "../styles";
+import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 const Navbar = () => {
   const [active, setActive] = useState("");
   return (
     <nav
-      className={`${styles.paddingx} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
-      <div className=" w-full flex justify-between items-center max-w-7xl mx-auto">
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
           to={"/"}
           className="flex items-center gap-2"
@@ -25,12 +25,28 @@ const Navbar = () => {
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
-          {navLinks.map((Link) => {
-            <li>
-              <a href={`#${Link.id}`}></a>
-            </li>;
-          })}
+          {navLinks.map((nav) => (
+            <li
+            key={nav.id}
+              className={`${
+                active === nav.title ? 'text-white' : 'text-secondary'
+              } hover:text-white text-[18px] font-medium cursor-pointer
+              `}
+              onClick={() => {setActive(nav.title); console.log(nav.title)}}>
+
+
+
+              <a href={`#${nav.id}`}>{nav.title}</a>
+              
+            </li>
+          ))}
+
         </ul>
+
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img src={menu} alt="menu" className="w-[28px] h-[28px]"/>
+        </div>
+
       </div>
     </nav>
   );
