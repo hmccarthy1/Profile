1f65ca2664c938212ae91320786cc64e967cc630
hmccarthy1 <huntermccarthy56@hotmail.com>
2024-02-25 22:01:34 -0600

    mobile nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,7 @@ import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
     <nav
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
@@ -44,7 +45,35 @@ const Navbar = () => {
         </ul>
 
         <div className="sm:hidden flex flex-1 justify-end items-center">
-          <img src={menu} alt="menu" className="w-[28px] h-[28px]"/>
+          <img src={ toggle ? close: menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer"
+          onClick={() => setToggle(!toggle)}
+          />
+
+          <div className={`${!toggle? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+
+              
+          <ul className="list-none flex flex-col justify-end items-start gap-4">
+          {navLinks.map((nav) => (
+            <li
+            key={nav.id}
+              className={`${
+                active === nav.title ? 'text-white' : 'text-secondary'
+              } hover:text-white text-[18px] font-medium cursor-pointer
+              `}
+              onClick={() => {setActive(nav.title); console.log(nav.title)}}>
+
+
+
+              <a href={`#${nav.id}`}>{nav.title}</a>
+              
+            </li>
+          ))}
+
+        </ul>
+
+
+          </div>
+
         </div>
 
       </div>
